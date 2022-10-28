c75ab19b7cf257da7512d3513e968f7c263afd7b
AST <32876857+AST9QT@users.noreply.github.com>
2022-10-28 21:26:15 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(5 * Y + X)
+})
+let Y = 0
+let X = 0
+radio.setGroup(5)
+X = 2
+Y = 2
+led.plot(X, Y)
 basic.forever(function () {
 	
 })
